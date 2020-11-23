80688fe53b1c1f867923e3d236f9f3a8e8b1bcc4
beastrc <makarov0114@outlook.com>
2020-11-23 20:05:29 -0300

    solves ATV-01

--- token-vesting/contracts/AlunaTokenVesting.sol
+++ token-vesting/contracts/AlunaTokenVesting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.12;
+pragma solidity 0.6.12;
 import "@openzeppelin/contracts-ethereum-package/contracts/drafts/TokenVesting.sol";
 
 contract AlunaTokenVesting is TokenVestingUpgradeSafe {
